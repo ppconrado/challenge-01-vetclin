0f1db9a835ab00e68fcdb0964daa5ce4158d1147
ppconrado <ppconrado@yahoo.com.br>
2023-07-27 01:57:39 -0300

    chore: mongoDB/.env setup

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,25 @@
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
-
+import { connectDB } from "./db/connect";
 import express, { Request, Response, NextFunction } from "express";
 import { json } from "body-parser";
 
 import tutorRoutes from "./routes/tutors";
 import petRoutes from "./routes/pets";
+
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/VetClin";
+
+const start = async () => {
+  try {
+    await connectDB(dbUrl);
+    console.log("DB connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
 
 const app = express();
 
